584c7bd6f8dd5ad5cda3a53cdaf5e2dcb5bae14f
philomonk <diepesters@web.de>
2016-09-16 13:10:53 +0100

    trial change deploy shell

--- _deploy.sh
+++ _deploy.sh
@@ -8,7 +8,7 @@ set -e
 git config --global user.email "K.R.Benoit@lse.ac.uk"
 git config --global user.name "Ken Benoit"
 
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+git clone -b gh-pages https://kbenoit@github.com/coursepack-bookdown.git book-output
 cd book-output
 cp -r ../_book/* ./
 git add --all *
