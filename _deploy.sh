147dc0978feec127f62c706f1890fa1a0284924c
philomonk <diepesters@web.de>
2016-09-16 13:44:49 +0100

    same

--- _deploy.sh
+++ _deploy.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-[ -z "${GITHUB_PAT}" ] && exit 0
+#[ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
 git config --global user.email "kbenoit@lse.ac.uk"
 git config --global user.name "kbenoit"
 
-git clone -b gh-pages https://github.com/kbenoit/coursepack-bookdown.git book-output
+git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
 cp -r ../_book/* ./
 git add --all *
