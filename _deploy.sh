9eaa37583903d6ae67196e42fe63d7ef6230844f
philomonk <diepesters@web.de>
2016-09-16 13:24:15 +0100

    same

--- _deploy.sh
+++ _deploy.sh
@@ -5,8 +5,8 @@ set -e
 #[ -z "${GITHUB_PAT}" ] && exit 0
 #[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "K.R.Benoit@lse.ac.uk"
-git config --global user.name "Ken Benoit"
+git config --global user.email "kbenoit@lse.ac.uk"
+git config --global user.name "kbenoit"
 
 git clone -b gh-pages https://github.com/kbenoit/coursepack-bookdown.git book-output
 cd book-output
